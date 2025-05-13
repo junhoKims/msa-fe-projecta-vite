5568723c691f8f7babc52a3147daaa0f79470905
krak <kraksin12@gmail.com>
2025-05-13 16:22:49 +0900

    feat: vite dev, build 환경 실행 가능하도록 구현

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "aProject",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
